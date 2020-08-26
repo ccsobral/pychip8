4e886aa0f2a22dbbc8a1ff294671b556ebcec982
ccsobral <cayo.c.sobral@gmail.com>
2020-08-26 18:36:51 +0100

    SDL Input now working

--- pychip8/chip8.py
+++ pychip8/chip8.py
@@ -1,7 +1,8 @@
-import tkinter as tk
 from time import perf_counter, sleep
 
 import numpy as np
+import sdl2
+import sdl2.ext
 
 from pychip8 import cli, cpu, display, key_input
 
@@ -12,18 +13,10 @@ class Chip8:
     def __init__(self):
         self._cli = cli.CmdLineInterface()
         self._args = self._cli.parsed_args
-
         self._cpu = cpu.Cpu()
-
-        #self._tk_manager = tk.Tk()
-        #self._tk_manager.title('pychip-8')
         self._display = display.Display(self._args['display_scale'])
         self._fps = 1.0 / 60.0
-
-        #self._key_input = key_input.KeyInput(
-        #    self._tk_manager, self._cpu.key_presses
-        #)
-
+        self._key_input = key_input.KeyInput(self._cpu.key_presses)
         self._open_rom()
 
     def _open_rom(self):
@@ -34,28 +27,20 @@ class Chip8:
         """Runs the emulator loop.
         """
 
-        self._exit_time = False
-        while not self._exit_time:
+        self._exit = False
+        while not self._exit:
             start_frame = perf_counter()
             delta_time = 0.0
-            '''
-            self._cpu.run_cycle()
-            if self._cpu.draw_flag:
-                np.copyto(self._cpu.draw_flag, False)
-                self._display.update_pixel_grid(self._cpu.pixel_buffer)
-            self._cpu.decrease_timers()
             while delta_time < self._fps:
-                delta_time = perf_counter() - start_frame
-            '''
-            while delta_time < self._fps:
+                self._key_input.process_events()
                 self._cpu.run_cycle()
                 if self._cpu.draw_flag:
                     np.copyto(self._cpu.draw_flag, False)
                     self._display.update_pixel_grid(self._cpu.pixel_buffer)
-                sleep(self._fps/4)
+                sleep(self._fps/6)
                 delta_time = perf_counter() - start_frame
             self._cpu.decrease_timers()
-            #self._exit_time = self._key_input.update_exit_status()
+            self._exit = self._key_input.update_exit_status()
 
     def __repr__(self):
         params = [f'{param}: {value!r}' for param, value in self._args.items()]
